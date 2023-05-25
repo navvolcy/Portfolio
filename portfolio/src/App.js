67966b0917a573a187e22951035f5afa116de21a
Nav L. Volcy <45085887+navvolcy@users.noreply.github.com>
2023-05-25 14:11:51 -0600

    portfolio : tailwindcsss working

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Works/>
+      <Hobbies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Ab0ut() {
+    return (
+        <div>About</div>
+    )
+}
+export default About
